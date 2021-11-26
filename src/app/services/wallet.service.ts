0331609d70ecd60940199904680bf036559a62b6
keeri <keerifox@users.noreply.github.com>
2021-11-26 17:38:38 +0000

    Update ticker to XNO, display XNO symbol where appropriate, refer to nano in lower case (#498)
    * remove unused duplicate string
    
    * replace ticker in the nano card
    
    * keypair generator: fix secret phrases being cropped on small viewports
    
    update terms to secret recovery seed/mnemonic
    
    * manage wallet - backup: update terms to secret recovery seed/mnemonic
    
    * new wallet: use existing translation for overwrite alert
    
    * "Nano" -> "nano" / "Ӿ"
    
    "seed/mnemonic" -> "secret recovery seed and/or mnemonic"
    account details: "wallet login" -> "wallet import"
    unit converter: NANO -> XNO
    import wallet: update wording to match "configure wallet" page
    
    * add symbol to account details balance, "NANO" -> "XNO"
    
    * fix ticker misplaced by 1px on both gecko and chromium
    
    * amount inputs: replace nf logo with xno symbol
    
    * "seed/mnemonic" -> "secret recovery seed/mnemonic"
    
    * remove unused array of wallet import options
    
    * "No login is needed" -> "No wallet import needed"
    
    * no private key in this field oops
    
    * account details: fix amount-fractional being 18px instead of 16px
    
    * use ticker XNO instead of symbol Ӿ in the auto-receive notification

--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -190,17 +190,17 @@ export class WalletService {
             // Incoming transaction
             if (this.addressBook.getTransactionTrackingById(addressLink)) {
               this.notifications.sendInfo(`Tracked address ${accountHrefLink} can now receive ${trackedAmount} PAW`, { length: 10000 });
-              console.log(`Tracked incoming block to: ${address} - ${trackedAmount} Nano`);
+              console.log(`Tracked incoming block to: ${address} - Ӿ${trackedAmount}`);
             }
             // Outgoing transaction
             if (this.addressBook.getTransactionTrackingById(address)) {
               this.notifications.sendInfo(`Tracked address ${accountHref} sent ${trackedAmount} PAW`, { length: 10000 });
-              console.log(`Tracked send block from: ${address} - ${trackedAmount} Nano`);
+              console.log(`Tracked send block from: ${address} - Ӿ${trackedAmount}`);
             }
           } else if (transaction.block.subtype === 'receive' && this.addressBook.getTransactionTrackingById(address)) {
             // Receive transaction
             this.notifications.sendInfo(`Tracked address ${accountHref} received incoming ${trackedAmount} PAW`, { length: 10000 });
-            console.log(`Tracked receive block to: ${address} - ${trackedAmount} Nano`);
+            console.log(`Tracked receive block to: ${address} - Ӿ${trackedAmount}`);
           } else if (transaction.block.subtype === 'change' && this.addressBook.getTransactionTrackingById(address)) {
             // Change transaction
             this.notifications.sendInfo(`Tracked address ${accountHref} changed its representative to ${rep}`, { length: 10000 });
